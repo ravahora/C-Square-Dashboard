288300ee002328dff0e18fcc3a8d4d4b8116a979
Rizwan Vahora <ravahora@gmail.com>
2021-07-20 12:30:56 +0530

    final

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,31 +4,31 @@ import { AdminLayoutComponent } from './layouts/admin/admin-layout.component';
 import { AuthLayoutComponent } from './layouts/auth/auth-layout.component';
 
 export const AppRoutes: Routes = [{
-            path: '',
-            redirectTo: 'dashboard',
-            pathMatch: 'full',
-        },
-        {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+},
+{
+    path: '',
+    component: AdminLayoutComponent,
+    children: [{
         path: '',
-        component: AdminLayoutComponent,
-        children: [{
-            path: '',
-            loadChildren: './dashboard/dashboard.module#DashboardModule'
-        },
-        {
-            path:'',
-            loadChildren:'./questions/questions.module#QuestionsModule'
-        },
-        {
-            path: 'manage',
-            loadChildren: './manage/manage.module#ManageModule',
-        },]
-        },{
-            path: '',
-            component: AuthLayoutComponent,
-            children: [{
-                path: 'pages',
-                loadChildren: './pages/pages.module#PagesModule'
-            }]
-        }
+        loadChildren: './dashboard/dashboard.module#DashboardModule'
+    },
+    {
+        path: '',
+        loadChildren: './questions/questions.module#QuestionsModule'
+    },
+    {
+        path: 'manage',
+        loadChildren: './manage/manage.module#ManageModule',
+    },]
+}, {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [{
+        path: 'pages',
+        loadChildren: './pages/pages.module#PagesModule'
+    }]
+}
 ];
